61fe49d57db1ef94bb5501f1eded73016f193fc5
Françuel Soares <fs.francuelsoares@gmail.com>
2020-01-20 22:45:45 -0300

    0.8.0
    # New - Added Style for Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,74 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>
+          Tênis de Caminhada Leve Confortável um titulo muito grande aqui
+        </strong>
+        <span>R$ 89,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>Adicionar ao Carinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis de Caminhada Leve Confortável</strong>
+        <span>R$ 89,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>Adicionar ao Carinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis de Caminhada Leve Confortável</strong>
+        <span>R$ 89,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>Adicionar ao Carinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis de Caminhada Leve Confortável</strong>
+        <span>R$ 89,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>Adicionar ao Carinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
