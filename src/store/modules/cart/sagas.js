77b0b35dc10359d6a987b96d6037d6abd61d63b1
Françuel Soares <fs.francuelsoares@gmail.com>
2020-02-20 22:23:03 -0300

    0.28.0
    # New - Added History Navigation

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -3,6 +3,7 @@ import { toast } from 'react-toastify';
 
 import { formatPrice } from '../../../utils/format';
 import api from '../../../services/api';
+import history from '../../../services/history';
 
 import { addToCartSuccess, updateAmountSuccess } from './actions';
 
@@ -36,6 +37,7 @@ function* addToCart({ id }) {
     };
 
     yield put(addToCartSuccess(data));
+    history.push('/cart');
   }
 }
 
